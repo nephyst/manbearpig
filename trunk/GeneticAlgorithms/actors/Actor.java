fdd7009a56de64529d0d9f9b5593e73285dc9c62
orangecrush <orangecrush@e405d028-9445-0410-aedf-731d3ec43665>
2008-03-15 18:46:26 +0000

    changes rules a bunch. way different now. really. dont believe me? look for yourself.

--- trunk/GeneticAlgorithms/actors/Actor.java
+++ trunk/GeneticAlgorithms/actors/Actor.java
@@ -11,60 +11,46 @@ public class Actor extends GameObject {
 	private int energy;
 
 	private int life;
-
-	private ArrayList<Rules> rules;
-
+	
+	private Rules rules;
+	
 	public Actor(int x, int y, ObjectType type, boolean isAlive) {
 		super(x, y, type, isAlive);
+		this.rules = new Rules();
 
-		this.rules = new ArrayList<Rules>();
-		rules.add(new Rules()); // generate random rules
 	}
 
 	public Actor(int x, int y, ObjectType type, boolean isAlive,
-			ArrayList<Rules> rules) {
+			Rules rules) {
 		super(x, y, type, isAlive);
 		this.rules = rules;
 	}
 
 	public Action getNextAction(Perception percept) {
-
-		if (this.isAlive() && this.getType() != ObjectType.FOOD) {
-			for (Rules r : this.rules) {
-				if (r.checkRule(percept)) {
-					return r.getAction();
-				}
-			}
-		}
-		return Action.NONE;
-
+		return rules.getAction(percept);
 	}
 
-	public Rules getRule(int i) {
-		return this.rules.get(i);
-	}
-
-	public Actor mutate(Actor other) {
-		ArrayList<Rules> newRule = new ArrayList<Rules>();
-		for (int i = 0; i < 4; i++) {
-			int r = (int) (Math.random() * 2);
-			if (r == 1) {
-				newRule.add(this.getRule(i));
-			} else {
-				newRule.add(other.getRule(i));
-			}
-		}
-		int r = (int) (Math.random() * 4);
-		newRule.add(newRule.get(r).mutate());
-		newRule.remove(r);
-		return new Actor(this.getX(), this.getY(), this.getType(), true,
-				newRule);
-	}
+//	public Actor mutate(Actor other) {
+//		ArrayList<Rules> newRule = new ArrayList<Rules>();
+//		for (int i = 0; i < 4; i++) {
+//			int r = (int) (Math.random() * 2);
+//			if (r == 1) {
+//			//	newRule.add(this.getRule(i));
+//			} else {
+//			//	newRule.add(other.getRule(i));
+//			}
+//		}
+//		int r = (int) (Math.random() * 4);
+//		//newRule.add(newRule.get(r).mutate());
+//		//newRule.remove(r);
+//		//return new Actor(this.getX(), this.getY(), this.getType(), true,
+//			//	newRule);
+//	}
 
 	public boolean canEat(GameObject other) {
 		boolean ret = true;
 		if (!other.isAlive()) {
-			ret = false;
+			//ret = false;
 		} else if (this.getType() == ObjectType.PREY &&
 				   other.getType() != ObjectType.FOOD) {
 			ret = false;
